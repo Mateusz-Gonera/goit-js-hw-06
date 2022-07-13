7ed3545c95e261671f31766dee06652c22b04798
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-07-13 17:30:44 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,5 @@
-console.log(1+2)
+const ulCategories = document.querySelector("ul#categories");
+const tagH2 = document.querySelector("li.item h2");
+console.log(`Number of categories: ${ulCategories.children.length}`);
+console.log(tagH2.textContent);
+console.log();
