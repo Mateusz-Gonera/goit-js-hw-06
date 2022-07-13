08926c7a2b6c8eabf2aea01679d17524ec091be5
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-07-13 23:03:50 +0200

    task 2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector("ul#ingredients");
+const item = () => {
+  for (const ingredient of ingredients) {
+    const liElem = document.createElement("li");
+    liElem.textContent = ingredient;
+    liElem.classList.add("item");
+    ul.append(liElem);
+  }
+ };
+console.log(item());
