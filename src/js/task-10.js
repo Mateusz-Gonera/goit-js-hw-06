bd7406d31de8105b569d2c571a7b254030e1fb98
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-07-14 22:11:06 +0200

    początek task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector("input");
+const boxes = document.querySelector("div#boxes");
+const amount = input.value;
+const createBoxes = (amount) => {
+  
+
+};
+console.log(boxes);
