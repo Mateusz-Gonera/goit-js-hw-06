fda17e086f5749b83d146b3d0e341b82b9a77860
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-07-14 14:45:25 +0200

    task 9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+const btn = document.querySelector("button.change-color");
+const text = document.querySelector("span.color");
+text.textContent = "#ffffff";
+body.style.backgroundColor = "#ffffff";
+btn.addEventListener("click", (event) => {
+  text.textContent = `${getRandomHexColor()}`;
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+})
+
