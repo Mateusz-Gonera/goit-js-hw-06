0d7ddd6df08bbdd7e01253e7c87ae761d48ce648
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-07-14 14:21:08 +0200

    task 8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,10 @@
-
+const form = document.querySelector("form.login-form");
+form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const { elements: { email, password } } = event.currentTarget;
+      if (email.value.length === 0 || password.value.length === 0) alert("Wszystkie pola muszą być uzupełnione");
+      if (email.value.length !== 0 || password.value.length !== 0) {
+            console.log({ email: email.value, password: password.value })
+      };
+      event.currentTarget.reset();
+})
